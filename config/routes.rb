ba7b7ec8c3b68a65f9738c2db3421d9c8268e3fd
hamerjjh <hamerjjh2@gmail.com$ git config --get user.namegit config --get user.namegit config --global user.email hamerjjh2@gmail.com>
2017-11-13 15:09:13 -0500

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    resources :posts do
+      resources :votes
+    end
+    resources :users do
+      
+        resources :votes
+  end
 end
+end 
