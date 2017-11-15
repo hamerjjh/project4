aebbfc5780238486432e8b3efa1f15bfc8c3e4ef
hamerjjh <hamerjjh2@gmail.com$ git config --get user.namegit config --get user.namegit config --global user.email hamerjjh2@gmail.com>
2017-11-15 14:26:01 -0500

    removed grey card background on postlist page

--- client/src/components/PostList.js
+++ client/src/components/PostList.js
@@ -20,22 +20,12 @@ const PostCard = styled.div`
     flex-direction: column;
     // width: 30%;
     font-size: 20px;
-    width: 300px;  height: 300px; margin: 40px auto;
+    width: 350px;  height: 300px; margin: 40px auto;
     text-align: center;
     justify-content: space-around;
     float: left;
     padding: 20px 10px 15px 10px;
-    background: rgba(216, 212, 212, 1);
-    border: 1px solid #fff;
-    -moz-box-shadow: 0px 2px 15px #333;
-    position: relative;
-    -webkit-transform: rotate(${props => props.rotationDegrees}deg);
     
-    -moz-transform: rotate(-8deg);
-    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-    margin-left: 10px;
-    margin-right: 10px;
-    margin-top: 20px;
     
     img {
         height: 80%;
